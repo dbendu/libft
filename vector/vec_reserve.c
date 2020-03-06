28ff52adf2523b7b863b11cc22fbe88381fe5ea6
sleng17 <sammert17@yandex.ru>
2020-03-06 20:45:26 +0300

    module vector done

--- vector/vec_reserve.c
+++ vector/vec_reserve.c
@@ -1,12 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vec_reserve.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbendu <dbendu@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/06 19:31:43 by dbendu            #+#    #+#             */
+/*   Updated: 2020/03/06 20:02:42 by dbendu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_vector.h"
 #include "private_vector.h"
 
 void			vec_reserve(void *vecptr, size_t elems)
 {
-	size_t		capacity;
+	t_vector	*vector;
+	size_t		total_cells;
 
-	vecptr = *(void**)vecptr;
-	capacity = ((t_vector*)(vecptr - sizeof(t_vector)))->capacity;
-	((t_vector*)(vecptr - sizeof(t_vector)))->size = elems;
-	((t_vector*)(vecptr - sizeof(t_vector)))->capacity = capacity - elems;
+	vector = *(void**)vecptr - sizeof(t_vector);
+	total_cells = vector->capacity + vector->size;
+	vector->size = elems;
+	vector->capacity = total_cells - elems;
 }
