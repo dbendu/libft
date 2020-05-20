f810de9268673d4f51f86a47e316938161d4b121
sleng17 <sammert17@yandex.ru>
2020-05-20 15:44:06 +0300

    simple one-byte memset and memcpy (temporary)

--- memory/ft_memcpy.c
+++ memory/ft_memcpy.c
@@ -6,60 +6,29 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/08 20:17:15 by dbendu            #+#    #+#             */
-/*   Updated: 2020/04/03 12:37:56 by user             ###   ########.fr       */
+/*   Updated: 2020/05/20 15:42:23 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stdio.h"
 #include "ft_memory.h"
 
-/*
-** num always multiple of __WORDSIZE
-*/
-
-static void		fast_memcpy(void *restrict dstptr,
-							const void *restrict srcptr,
-							size_t num)
-{
-	register t_int_ws	*dst;
-	register t_int_ws	*src;
-
-	num /= WORD_SIZE_BYTES;
-	dst = (t_int_ws*)dstptr;
-	src = (t_int_ws*)srcptr;
-	while (num > 1)
-	{
-		*dst = *src;
-		dst[1] = src[1];
-		dst += 2;
-		src += 2;
-		num -= 2;
-	}
-	if (num)
-		*dst = *src;
-}
-
 void			*ft_memcpy(void *restrict dstptr,
 							const void *restrict srcptr,
 							size_t bytes)
 {
-	void		*dst;
+	unsigned char *restrict	dst;
+	unsigned char *restrict	src;
 
-	dst = dstptr;
-	if (bytes > 64)
+	dst = (unsigned char *restrict)dstptr;
+	src = (unsigned char *restrict)srcptr;
+	while (bytes)
 	{
-		fast_memcpy(dstptr, srcptr, bytes & ~(WORD_SIZE_BYTES - 1));
-		dstptr += bytes & ~(WORD_SIZE_BYTES - 1);
-		srcptr += bytes & ~(WORD_SIZE_BYTES - 1);
-		bytes &= (WORD_SIZE_BYTES - 1);
+		*dst = *src;
+		dst += 1;
+		src += 1;
+		bytes -= 1;
 	}
-	while (bytes > 1)
-	{
-		*(char*)dstptr++ = *(char*)srcptr++;
-		*(char*)dstptr++ = *(char*)srcptr++;
-		bytes -= 2;
-	}
-	if (bytes)
-		*(char*)dstptr = *(char*)srcptr;
+
 	return (dst);
 }
--- memory/ft_memset.c
+++ memory/ft_memset.c
@@ -6,53 +6,23 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/08 20:17:43 by dbendu            #+#    #+#             */
-/*   Updated: 2020/04/03 12:36:16 by user             ###   ########.fr       */
+/*   Updated: 2020/05/20 15:39:52 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_memory.h"
 
-static void		fast_memset(void *restrict dstptr, char c, size_t bytes)
-{
-	register t_int_ws	*dst;
-	t_int_ws			buf;
-	int					iter;
-
-	buf = 0;
-	iter = -1;
-	while (++iter < WORD_SIZE_BYTES)
-		buf += c << ((WORD_SIZE_BYTES - 1 - iter) * 8);
-	bytes /= WORD_SIZE_BYTES;
-	dst = (t_int_ws*)dstptr;
-	while (bytes > 1)
-	{
-		*dst = buf;
-		dst[1] = buf;
-		dst += 2;
-		bytes -= 2;
-	}
-	if (bytes)
-		*dst = buf;
-}
-
 void			*ft_memset(void *restrict dest, int c, size_t bytes)
 {
-	void		*dst;
+	unsigned char *restrict	mem;
+	const unsigned char		value = c;
 
-	dst = dest;
-	if (bytes > 128)
+	mem = (unsigned char *restrict)dest;
+	while (bytes)
 	{
-		fast_memset(dest, c, bytes & ~(WORD_SIZE_BYTES - 1));
-		dest += bytes & ~(WORD_SIZE_BYTES - 1);
-		bytes &= (WORD_SIZE_BYTES - 1);
+		*mem = value;
+		mem += 1;
+		bytes -= 1;
 	}
-	while (bytes > 1)
-	{
-		*(char*)dest++ = c;
-		*(char*)dest++ = c;
-		bytes -= 2;
-	}
-	if (bytes)
-		*(char*)dest = c;
-	return (dst);
+	return (dest);
 }
