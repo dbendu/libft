a393b02522dc66f75bbb1815bca81bf93083a6e7
sleng17 <sammert17@yandex.ru>
2020-05-29 16:13:26 +0300

    stupid author cant write fast memcpy/set

--- memory/ft_memcpy.c
+++ memory/ft_memcpy.c
@@ -6,21 +6,21 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/08 20:17:15 by dbendu            #+#    #+#             */
-/*   Updated: 2020/05/28 15:05:22 by user             ###   ########.fr       */
+/*   Updated: 2020/05/29 16:12:53 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stdio.h"
 #include "ft_memory.h"
 
-static void	copy_bytes(void **dstptr, const void *restrict *srcptr,
-						size_t bytes)
+void			*ft_memcpy(void *restrict dstptr,
+							const void *restrict srcptr,
+							size_t bytes)
 {
-	t_byte	*dst;
-	t_byte	*src;
+	const t_byte	*src = (t_byte*)srcptr;
+	t_byte			*dst;
 
-	dst = *(t_byte**)dstptr;
-	src = *(t_byte**)srcptr;
+	dst = (t_byte*)dstptr;
 	while (bytes)
 	{
 		*dst = *src;
@@ -28,51 +28,5 @@ static void	copy_bytes(void **dstptr, const void *restrict *srcptr,
 		src += 1;
 		bytes -= 1;
 	}
-	*dstptr = dst;
-	*srcptr = src;
-}
-
-static void	copy_words(void **dstptr, const void *restrict *srcptr,
-						size_t words)
-{
-	t_int_ws	*dst;
-	t_int_ws	*src;
-
-	dst = *(t_int_ws**)dstptr;
-	src = *(t_int_ws**)srcptr;
-	while (words)
-	{
-		*dst = *src;
-		dst += 1;
-		src += 1;
-		words -= 1;
-	}
-	*dstptr = dst;
-	*srcptr = src;
-}
-
-void			*ft_memcpy(void *restrict dstptr,
-							const void *restrict srcptr,
-							size_t bytes)
-{
-	const size_t	word_sz_bytes = WORD_SIZE_BYTES;
-	void			*dst;
-	size_t			bytes_;
-
-	dst = dstptr;
-	bytes_ = (size_t)dst & (word_sz_bytes - 1);
-	if (bytes_)
-	{
-		copy_bytes(&dst, &srcptr, word_sz_bytes - bytes_);
-		bytes -= word_sz_bytes - bytes_;
-	}
-	if (bytes > (word_sz_bytes << 2))
-	{
-		bytes_ = bytes & (~(word_sz_bytes - 1));
-		copy_words(&dst, &srcptr, bytes_ >> WORD_POW2);
-		bytes -= bytes_;
-	}
-	if (bytes)
-		copy_bytes(&dst, &srcptr, bytes);
 	return (dstptr);
 }
--- memory/ft_memset.c
+++ memory/ft_memset.c
@@ -6,69 +6,23 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/08 20:17:43 by dbendu            #+#    #+#             */
-/*   Updated: 2020/05/28 15:18:16 by user             ###   ########.fr       */
+/*   Updated: 2020/05/29 16:13:07 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_memory.h"
 
-static void		set_bytes(void **dstptr, t_byte value, size_t bytes)
+void			*ft_memset(void *restrict dstptr, int c, size_t bytes)
 {
-	t_byte	*dst;
+	const t_byte	value = c;
+	t_byte			*dst;
 
-	dst = *(t_byte**)dstptr;
+	dst = (t_byte*)dstptr;
 	while (bytes)
 	{
 		*dst = value;
 		dst += 1;
 		bytes -= 1;
 	}
-	*dstptr = dst;
-}
-
-static void		set_words(void **dstptr, t_byte value, size_t words)
-{
-	t_int_ws	*dst;
-	t_int_ws	word;
-	int			i;
-
-	i = 0;
-	while (i < WORD_SIZE_BYTES)
-	{
-		((char*)&word)[i] = value;
-		i += 1;
-	}
-	dst = *(t_int_ws**)dstptr;
-	while (words)
-	{
-		*dst = word;
-		dst += 1;
-		words -= 1;
-	}
-	*dstptr = dst;
-}
-
-void			*ft_memset(void *restrict dstptr, int c, size_t bytes)
-{
-	const size_t	word_sz_bytes = WORD_SIZE_BYTES;
-	const t_byte	value = c;
-	void			*dst;
-	size_t			bytes_;
-
-	dst = dstptr;
-	bytes_ = (size_t)dst & (word_sz_bytes - 1);
-	if (bytes_)
-	{
-		set_bytes(&dst, value, word_sz_bytes - bytes_);
-		bytes -= word_sz_bytes - bytes_;
-	}
-	if (bytes > (word_sz_bytes << 2))
-	{
-		bytes_ = bytes & (~(word_sz_bytes - 1));
-		set_words(&dst, value, bytes_ >> WORD_POW2);
-		bytes -= bytes_;
-	}
-	if (bytes)
-		set_bytes(&dst, value, bytes);
 	return (dstptr);
 }
